0f20575d2b35580ec8e2fc2b6a2868fbaf3ddf02
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2025-02-17 15:09:02 +0530

    Primitive data types

--- main.rs
+++ main.rs
@@ -1,3 +1,28 @@
+// Primitive data types
+
 fn main(){
-    println!("Hello, World!");
+    // 1. Integers
+    let x:i32 = -10;
+    let y:u32 = 20;
+    println!("x = {}", x);
+    println!("y = {}", y);
+    
+    
+    // 2. Floating point numbers
+    let f1: f32 = 1.0;
+    let f2: f64 = 2.0;
+    println!("f1 = {} {}", f1, f2);
+    println!("f2 = {}", f2);
+
+    // 3. Boolean
+    let b1: bool = true;
+    let b2: bool = false;
+    println!("b1 = {}", b1);
+    println!("b2 = {}", b2);
+
+    // 4. Characters
+    let c1: char = 'a';
+    let c2: char = 'b';
+    println!("c1 = {}", c1);
+    println!("c2 = {}", c2);
 }
