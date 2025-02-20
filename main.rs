31a2e9cac6e09883cbd4ff771a25abdc5962f3da
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2025-02-20 19:15:32 +0530

    Compound Data types

--- main.rs
+++ main.rs
@@ -25,4 +25,31 @@ fn main(){
     let c2: char = 'b';
     println!("c1 = {}", c1);
     println!("c2 = {}", c2);
+
+    // Compound Data types
+    // Array, tuples, slices, and strings(slice string)
+
+    // 1. Arrays 
+    let numbers:[i32; 5] = [1,2,3,4,5];
+    println!("Numbers: {:?}",numbers);
+    // ////////////////////////////////////////////////
+
+    // 2. Tuples
+    let tuple: (&str, i32, bool) = ("Alice", 20, true);
+    println!("Tuple: {:?}", tuple);
+    // ////////////////////////////////////////////////
+
+    // 3. Slices
+    let num_slice: &[i32] = &[1,2,3,4,5];
+    println!("Slice: {:?}", num_slice);
+    // ////////////////////////////////////////////////
+
+    // 4. Strings
+    let str1: &str = "Hello";
+    let str2: String = String::from("World");
+    println!("String1: {}", str1);
+    println!("String2: {}", str2);
+    // ////////////////////////////////////////////////
+
+
 }
